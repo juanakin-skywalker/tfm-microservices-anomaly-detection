2a64e8760d8ba33fd88d55d0161e3d7fb79104f1
juanakin-skywalker <juanan.alcaraz@gmail.com>
2026-07-12 02:16:10 +0200

    Creado notebook para transformacion de datos y creada tabla metricas_transformed

--- server1/database_creation.sql
+++ server1/database_creation.sql
@@ -2,10 +2,14 @@
 -- SCRIPT DE CONFIGURACIÓN DE BASE DE DATOS - TIMESCALEDB (TFM)
 -- ============================================================================
 
+
+
+
 -- 1. Asegurar que la extensión de TimescaleDB está activa en la base de datos
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
--- 2. Creación de la tabla principal de métricas
+-- 2. Tabla principal de métricas(metricas)
+-- 2.1. Creación de la tabla principal de métricas
 -- Se utiliza el tipo DOUBLE PRECISION para garantizar la máxima precisión en valores métricos.
 CREATE TABLE IF NOT EXISTS metricas (
     time            TIMESTAMPTZ NOT NULL,   
@@ -19,27 +23,73 @@ CREATE TABLE IF NOT EXISTS metricas (
     label           TEXT           
 );
 
--- 3. Transformar la tabla convencional en una Hypertable de TimescaleDB
+-- 2.2. Transformar la tabla convencional en una Hypertable de TimescaleDB
 -- Esto activa el particionado automático por tiempo en bloques optimizados de 1 día.
 SELECT create_hypertable('metricas', 'time', if_not_exists => TRUE, chunk_time_interval => INTERVAL '1 day');
 
--- 4. Creación de índices optimizados para Machine Learning e Ingesta masiva
+-- 2.3. Creación de índices optimizados para Machine Learning e Ingesta masiva
 -- Se incluye 'execution_name' en el índice compuesto para acelerar radicalmente las 
 -- búsquedas cuando los scripts de Python filtren una simulación específica y un tipo 
 -- de métrica ordenado cronológicamente para alimentar el modelo predictivo.
 CREATE INDEX IF NOT EXISTS idx_metricas_exec_name_time 
 ON metricas (execution_name, metric_name, time DESC);
 
--- 5. Índice opcional para facilitar la segmentación por etiquetas de anomalías
+-- 2.4. Índice opcional para facilitar la segmentación por etiquetas de anomalías
 CREATE INDEX IF NOT EXISTS idx_metricas_label 
 ON metricas (label) 
 WHERE label IS NOT NULL;
 
 
 
--- 6. Configuración de la compresion de la tabla métricas, no será automática, habrá que lanzarla manualmente
+-- 2.5. Configuración de la compresion de la tabla métricas, no será automática, habrá que lanzarla manualmente
 ALTER TABLE metricas SET (
     timescaledb.compress,
     timescaledb.compress_segmentby = 'execution_name, metric_name, instance',
     timescaledb.compress_orderby = 'time DESC'
 );
+
+
+
+
+
+-- 3. Tabla de métricas transformadas(interpoladas a muestro uniforme, eliminadas metricas redundantes y rellenados nulos)(metricas)
+-- 3.1. Creación de la tabla principal de métricas
+-- Se utiliza el tipo DOUBLE PRECISION para garantizar la máxima precisión en valores métricos.
+CREATE TABLE IF NOT EXISTS metricas_transformed (
+    time            TIMESTAMPTZ NOT NULL,   
+    execution_name  TEXT,                  -- Identificador único de cada simulación o ejecución del pipeline
+    instance        TEXT,                  
+    grupo           TEXT,                  
+    job             TEXT,                  
+    metric_name     TEXT,           
+    metric_value    DOUBLE PRECISION,                
+    tags            JSONB,                  
+    label           TEXT           
+);
+
+-- 3.2. Transformar la tabla convencional en una Hypertable de TimescaleDB
+-- Esto activa el particionado automático por tiempo en bloques optimizados de 1 día.
+SELECT create_hypertable('metricas_transformed', 'time', if_not_exists => TRUE, chunk_time_interval => INTERVAL '1 day');
+
+-- 3.3. Creación de índices optimizados para Machine Learning e Ingesta masiva
+-- Se incluye 'execution_name' en el índice compuesto para acelerar radicalmente las 
+-- búsquedas cuando los scripts de Python filtren una simulación específica y un tipo 
+-- de métrica ordenado cronológicamente para alimentar el modelo predictivo.
+CREATE INDEX IF NOT EXISTS idx_metricas_transformed_exec_name_time 
+ON metricas_transformed (execution_name, metric_name, time DESC);
+
+-- 3.4. Índice opcional para facilitar la segmentación por etiquetas de anomalías
+CREATE INDEX IF NOT EXISTS idx_metricas_transformed_label 
+ON metricas_transformed (label) 
+WHERE label IS NOT NULL;
+
+
+
+-- 3.5. Configuración de la compresion de la tabla métricas, no será automática, habrá que lanzarla manualmente
+ALTER TABLE metricas_transformed SET (
+    timescaledb.compress,
+    timescaledb.compress_segmentby = 'execution_name, metric_name, instance',
+    timescaledb.compress_orderby = 'time DESC'
+);
+
+
